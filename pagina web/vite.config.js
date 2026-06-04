a2d79f8991c4872796b57a596a261a969242f394
Juan Pablo Anuard Vazquez Guzman <juanpablovazques544@gmail.com>
2026-06-04 13:35:32 -0600

    con deployment

--- pagina web/vite.config.js
+++ pagina web/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Cambia '/be-fisioterapia/' por el nombre de tu repositorio en GitHub
-// Ejemplo: si tu repo se llama "mi-web", pon base: '/mi-web/'
 export default defineConfig({
   plugins: [react()],
-  base: '/be-fisioterapia/',
+  base: '/BE-FISIOTERAPIA/pagina web/',
 })
